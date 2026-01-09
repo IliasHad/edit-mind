aa127fb8befaa6d2290e0aecdc3c1ee57169e994
Ilias Haddad <iliasshaddad18@gmail.com>
2026-01-09 16:04:00 +0100

    fix remove old config parser

--- apps/background-jobs/src/config.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-  port: process.env.BACKGROUND_JOBS_PORT || 4000,
-  databaseUrl: process.env.DATABASE_URL || '',
-  redisHost: process.env.REDIS_HOST || 'redis',
-  redisPort: parseInt(process.env.REDIS_PORT || '6379'),
-}
