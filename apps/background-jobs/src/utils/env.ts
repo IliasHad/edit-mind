9d9fe5922732b38cdbe5993de92eda460a978a9d
Ilias Haddad <iliasshaddad18@gmail.com>
2026-01-09 16:02:50 +0100

    feat: add zod env variables parser

---
+++ apps/background-jobs/src/utils/env.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+  SESSION_SECRET: z.string().min(1, 'SESSION_SECRET is required'),
+  WEB_APP_URL: z.string().min(1, 'WEB_APP_URL is required'),
+  REDIS_HOST: z.string().min(1, 'REDIS_HOST is required'),
+  REDIS_PORT: z
+    .string()
+    .transform((val) => Number(val))
+    .refine((val) => !Number.isNaN(val) && val > 0, {
+      message: 'REDIS_PORT must be a valid number',
+    }),
+  DATABASE_URL: z.string().min(1, 'DATABASE_URL is required'),
+  BACKGROUND_JOBS_PORT: z
+    .string()
+    .transform((val) => Number(val))
+    .refine((val) => !Number.isNaN(val) && val > 0, {
+      message: 'BACKGROUND_JOBS_PORT must be a valid number',
+    }),
+})
+
+export const env = envSchema.parse(process.env)
