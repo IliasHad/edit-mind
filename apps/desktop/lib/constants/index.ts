cfff6cfe0d9b657a6d00faa8906bfdc43c728fd0
Ilias Haddad <iliasshaddad18@gmail.com>
2026-02-01 13:48:26 +0100

    remove desktop app because we're fully moving to docker

--- apps/desktop/lib/constants/index.ts
+++
@@ -1,6 +0,0 @@
-
-import 'dotenv/config'
-
-// General
-export const IS_WIN = process.platform === 'win32';
-
--- apps/desktop/lib/conveyor/handlers/app-handler.ts
+++
@@ -1,7 +0,0 @@
-import { type App, type WebContents } from 'electron'
-import { handle, sender } from '@/lib/main/shared'
-
-export const registerAppHandlers = (app: App, webContents: WebContents) => {
-  const send = sender(webContents)
-  handle('version', () => app.getVersion())
-}
