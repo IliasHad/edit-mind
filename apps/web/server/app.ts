315ff988e03e1048fc784e3ad976ff23b3e5f686
Ilias Haddad <iliasshaddad18@gmail.com>
2026-03-16 23:45:36 +0000

    fix: web app and background jobs proxy issue

--- apps/web/server/app.ts
+++ apps/web/server/app.ts
@@ -2,17 +2,17 @@ import 'react-router'
 import { createRequestHandler } from '@react-router/express'
 import { createProxyMiddleware } from 'http-proxy-middleware'
 import express from 'express'
+import { env } from '../app/env'
 
 export const app = express()
 
 app.use(
   '/internal',
   createProxyMiddleware({
-    target: process.env.BACKGROUND_JOBS_URL,
+    router: () => `${env.BACKGROUND_JOBS_URL}/internal`,
     changeOrigin: true,
   })
 )
-
 app.use(
   createRequestHandler({
     build: () => import('virtual:react-router/server-build'),
